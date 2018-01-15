6edf0e8d761622840e19fc2bb863bbee5034cc4a
R NK <right.rcon.f@outlook.com>
2018-01-15 18:45:46 +0900

    複数ワードで検索できるように変更

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Book;
 
 
 class SearchController extends Controller
@@ -12,12 +13,26 @@ class SearchController extends Controller
     	return view('searchtop');
     }
 
-    
-
     public function search(Request $request) {
     	$this->validate($request, ['searchword' => 'required']);
     	$searchword = $request->input('searchword');
-    	$books = DB::table('books')->where('name','like','%'.$searchword.'%')->paginate(16);
+    	//全角空白があったら半角空白にそろえる
+		$words = str_replace("　", " ", $searchword);
+		//空白文字で検索ワードを分割	
+		$word_array = preg_split("/[ ]+/",$words);
+
+		$query = Book::query();
+
+		foreach ($word_array as $word) {
+			$query->where('name', 'like', '%'.$word.'%');
+		}
+		if($query->count() == 0) {
+			foreach ($word_array as $word) {
+				$query->orwhere('name', 'like', '%'.$word.'%');
+			}
+		}
+		$books = $query->paginate(16);
+
     	return view('list', compact('books', 'searchword'));
     }
 }
