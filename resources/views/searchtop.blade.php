389a9d6b65d3b595e51bb8c02817af13781b5e54
R-NK <right.rcon.f@outlook.com>
2018-01-16 08:26:55 +0900

    検索トップページのGridを調整

--- resources/views/searchtop.blade.php
+++ resources/views/searchtop.blade.php
@@ -12,30 +12,26 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/top.css') }}">
 
 <body>  
-  <div class="site-wrapper">
-    <div class="site-wrapper-inner">
-      <div class="container">
-        <div class="masthead clearfix">
-          <div class="container inner">
-            <h3 class="wf-sawarabimincho">Y書店</h3>
-          </div>
-        </div>
-        <div class="inner cover">
-          <h1 class="cover-heading wf-sawarabimincho title">在庫検索</h1>
-          <p class="lead wf-sawarabimincho content">本のない部屋は、魂のない肉体のようなものだ。</p>
-          <form action="searchresult">
-            <div class="row">
-            <input type="text" name="searchword" class="form-control col-sm-11">
-            <button class="btn btn-secondary col-sm-1" type="submit"><i class="fa fa-search"></i></button>
-          </div>
-          </form>
-        </div>
-
-      </div>
-
-    </div>
-
-  </div>
-
+	<div class="site-wrapper">
+		<div class="site-wrapper-inner">
+			<div class="container">
+				<div class="masthead clearfix">
+					<div class="container inner">
+						<h3 class="wf-sawarabimincho">Y書店</h3>
+					</div>
+				</div>
+				<div class="inner cover">
+					<h1 class="cover-heading wf-sawarabimincho title">在庫検索</h1>
+					<p class="lead wf-sawarabimincho content">本のない部屋は、魂のない肉体のようなものだ。</p>
+					<form action="searchresult">
+						<div class="row">
+							<input type="text" name="searchword" class="form-control col-11">
+							<button class="btn btn-secondary col-1" type="submit"><i class="fa fa-search"></i></button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
