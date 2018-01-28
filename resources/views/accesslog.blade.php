16025c11e62e97d6d203e7a9fb55516f8b924cc2
R-NK <right.rcon.f@outlook.com>
2018-01-28 22:05:14 +0900

    accesslogのmigrationを追加

--- resources/views/accesslog.blade.php
+++ resources/views/accesslog.blade.php
@@ -12,9 +12,60 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		<h1>なにもないよ</h1>
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+	<li class="nav-item">
+		<a class="nav-link active" id="current-tab" data-toggle="tab" href="#current" role="tab" aria-controls="current" aria-selected="true">現在のデータ</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="past-tab" data-toggle="tab" href="#past" role="tab" aria-controls="past" aria-selected="false">過去のデータ</a>
+	</li>
+</ul>
+<div class="tab-content" id="myTabContent">
+	<div class="tab-pane fade show active" id="current" role="tabpanel" aria-labelledby="current-tab">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card box mt-4">
+						<div id="chart_word_current"></div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card box mt-4">
+						<div id="chart_count_current"></div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card box mt-4">
+						<div id="chart_word_multi"></div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card box mt-4">
+						<div id="chart_no_hit_word"></div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
+	<div class="tab-pane fade" id="past" role="tabpanel" aria-labelledby="past-tab">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card box mt-4">
+						<div id="chart_word_preview"></div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card box mt-4">
+						<div id="chart_count_preview"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
 
 @section('script')
